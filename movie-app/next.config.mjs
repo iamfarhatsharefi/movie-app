a50bb911eeb6ff96697152e64b7886441b05992c
iamfarhatsharefi <sharefifarhat@gmail.com>
2024-10-08 11:13:03 -0700

    updating code for netlify editing

--- movie-app/next.config.mjs
+++ movie-app/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    trailingSlash: true,
+    exportPathMap: async function (
+      defaultPathMap,
+      { dev, dir, outDir, distDir, buildId }
+    ) {
+      // Define custom export paths if needed
+      return {
+        '/': { page: '/' },
+        // Add other routes or dynamic ones if required
+      };
+    },
+  };
+  
+  export default nextConfig;
+  
